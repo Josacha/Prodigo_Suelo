d9b37d9ca187ab6b290154c77baad7c57a454d28
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-12 17:24:24 -0600

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,18 +1,14 @@
 import { auth, db } from "./firebase.js";
-
 import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
 import { doc, getDoc } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// redirección compatible con GitHub Pages
 function go(page) {
   const base = location.pathname.split("/")[1];
   window.location.href = `/${base}/${page}`;
 }
 
-// PROTECCIÓN TOTAL
 export function protegerPagina(rolPermitido) {
   onAuthStateChanged(auth, async (user) => {
 
@@ -22,7 +18,6 @@ export function protegerPagina(rolPermitido) {
       return;
     }
 
-    // 🔍 BUSCAR ROL
     const ref = doc(db, "usuarios", user.uid);
     const snap = await getDoc(ref);
 
@@ -37,8 +32,10 @@ export function protegerPagina(rolPermitido) {
     // ❌ ROL INCORRECTO
     if (rolUsuario !== rolPermitido) {
       go("index.html");
+      return;
     }
 
-    // ✅ SI TODO ESTÁ BIEN → NO HACE NADA
+    // ✅ AUTORIZADO → MOSTRAR PÁGINA
+    document.body.style.display = "block";
   });
 }
