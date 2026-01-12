d3d3b76769aa802b46feeffabe19aa2cd86f0101
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-12 16:20:25 -0600

    Update guard.js

--- js/guard.js
+++ js/guard.js
@@ -1,7 +1,8 @@
-import { onAuthStateChanged }
+/*import { onAuthStateChanged }
 from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { auth } from "./firebase.js";
 
 onAuthStateChanged(auth, user => {
   if (!user) location.href = "index.html";
 });
+
